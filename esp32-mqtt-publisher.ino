bde2381a7c2e8d5a26faced3dd6f99b1b4cbc48b
JAFB321 <jafb321@gmail.com>
2021-10-12 22:25:36 -0700

    feat: setup mqtt client

--- esp32-mqtt-publisher.ino
+++ esp32-mqtt-publisher.ino
@@ -5,6 +5,13 @@
 const char* ssid = "WIFI SSID";
 const char* pass = "WIFI PASSWORD";
 
+// MQTT Broker 
+const char* mqtt_server = "192.168.1.50";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 void setup()
 {
 	Serial.begin(9600);
@@ -21,7 +28,14 @@ void setup()
     Serial.println("1.- Connected");
     Serial.println(WiFi.localIP());
     Serial.println();
+
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(on_message);
 }
+
+void on_message(char* topic, byte* msg, unsigned int length){
+    Serial.println("A message has been received");
+ }
 
 void loop()
 {
